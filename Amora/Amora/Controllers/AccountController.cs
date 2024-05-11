c3065e726c0f10fa9457374aed06c9da1bde5f13
jakubtrznadel <jakub.trznadel@studenci.collegiumwitelona.pl>
2024-05-11 18:05:10 +0200

    Amora

---
+++ Amora/Amora/Controllers/AccountController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Amora.Data;
+using System.Security.Cryptography;
+using Amora.Models;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using System.Diagnostics;
+
+namespace Amora.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly AmoraContext _context;
+
+        public AccountController(AmoraContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Register()
+        {
+            return View("~/Views/Home/Register.cshtml");
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //string hashedPassword = HashPassword(model.Password);
+
+                try
+                {
+                    var user = new RegisterViewModel
+                    {
+                        Email = model.Email,
+                        Name = model.Name,
+                        Surname = model.Surname,
+                        Gender = model.Gender,
+                        Age = model.Age,
+                        PhoneNumber = model.PhoneNumber,
+                        Hobby = model.Hobby,
+                        Password = model.Password,
+                        ConfirmPassword = model.ConfirmPassword
+                    };
+
+                    _context.RegisterViewModel.Add(user);
+                    _context.SaveChanges();
+
+                    return RedirectToAction("Index", "Home");
+                }
+                catch (Exception ex)
+                {
+                    // Obsługa błędów zapisu do bazy danych
+                    ModelState.AddModelError("", "Wystąpił błąd podczas rejestracji użytkownika. " + ex.Message); // Dodanie komunikatu błędu do ModelState
+                    // Logowanie lub inne działania w przypadku błędu
+                    return View(model);
+                }
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ Amora/Amora/Controllers/RegisterViewModelsController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Amora.Data;
+using Amora.Models;
+
+namespace Amora.Controllers
+{
+    public class RegisterViewModelsController : Controller
+    {
+        private readonly AmoraContext _context;
+
+        public RegisterViewModelsController(AmoraContext context)
+        {
+            _context = context;
+        }
+
+        // GET: RegisterViewModels
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.RegisterViewModel.ToListAsync());
+        }
+
+        // GET: RegisterViewModels/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var registerViewModel = await _context.RegisterViewModel
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (registerViewModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(registerViewModel);
+        }
+
+        // GET: RegisterViewModels/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RegisterViewModels/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Email,Name,Surname,Gender,Age,PhoneNumber,Hobby,Password,ConfirmPassword")] RegisterViewModel registerViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(registerViewModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(registerViewModel);
+        }
+
+        // GET: RegisterViewModels/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var registerViewModel = await _context.RegisterViewModel.FindAsync(id);
+            if (registerViewModel == null)
+            {
+                return NotFound();
+            }
+            return View(registerViewModel);
+        }
+
+        // POST: RegisterViewModels/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Email,Name,Surname,Gender,Age,PhoneNumber,Hobby,Password,ConfirmPassword")] RegisterViewModel registerViewModel)
+        {
+            if (id != registerViewModel.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(registerViewModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RegisterViewModelExists(registerViewModel.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(registerViewModel);
+        }
+
+        // GET: RegisterViewModels/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var registerViewModel = await _context.RegisterViewModel
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (registerViewModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(registerViewModel);
+        }
+
+        // POST: RegisterViewModels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var registerViewModel = await _context.RegisterViewModel.FindAsync(id);
+            if (registerViewModel != null)
+            {
+                _context.RegisterViewModel.Remove(registerViewModel);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RegisterViewModelExists(int id)
+        {
+            return _context.RegisterViewModel.Any(e => e.Id == id);
+        }
+    }
+}
