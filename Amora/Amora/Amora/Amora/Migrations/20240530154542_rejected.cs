49e467f01cf0b49c936fd31c06680d2550229408
jakubtrznadel <jakub.trznadel@studenci.collegiumwitelona.pl>
2024-05-30 18:16:05 +0200

    amora

---
+++ Amora/Amora/Amora/Amora/Migrations/20240530154542_rejected.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Amora.Migrations
+{
+    /// <inheritdoc />
+    public partial class rejected : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Rejected",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    User_ID = table.Column<int>(type: "int", nullable: false),
+                    RejectedUser_ID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rejected", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Rejected");
+        }
+    }
+}
---
+++ Amora/Amora/Amora/Amora/Models/MatchAccepted.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Amora.Models
+{
+    public class MatchAccepted
+    {
+        public int Id { get; set; }
+        public int User_Id { get; set; }
+        public int MatchedUser_Id { get; set; }
+
+    }
+}
